0cc2f59ee361e0c0f73ed33bcd33aa36d9d8098b
Robert Nordan <robert.nordan@norkart.no>
2013-09-17 13:58:11 +0200

    Check that shove gesture is proper
    Now shove actions are only initiated if the fingers are within 20
    degrees of each other.

--- src/com/almeros/android/multitouch/gesturedetectors/ShoveGestureDetector.java
+++ src/com/almeros/android/multitouch/gesturedetectors/ShoveGestureDetector.java
@@ -5,6 +5,24 @@ import android.view.MotionEvent;
 
 /**
  * @author Robert Nordan (robert.nordan@norkart.no)
+ * 
+ * Copyright (c) 2013, Norkart AS
+
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *  in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
  */
 public class ShoveGestureDetector extends TwoFingerGestureDetector {
 
@@ -156,6 +174,20 @@ public class ShoveGestureDetector extends TwoFingerGestureDetector {
 		float cy1 = curr.getY(1);
 		mCurrAverageY = (cy0 + cy1) / 2.0f;
 	}
+    
+    @Override
+    protected boolean isSloppyGesture(MotionEvent event){
+    	boolean sloppy = super.isSloppyGesture(event);
+    	if (sloppy)
+    		return true;
+    	
+    	// If it's not traditionally sloppy, we check if the angle between fingers
+    	// is acceptable.
+    	double angle = Math.abs(Math.atan2(mCurrFingerDiffY, mCurrFingerDiffX));
+    	//about 20 degrees, left or right
+    	return !(( 0.0f < angle && angle < 0.35f)
+    			|| 2.79f < angle && angle < Math.PI); 
+    }
 
 
     /**
